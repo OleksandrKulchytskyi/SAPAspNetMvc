95039dde038f3c294ca7e44aef5a66540ffd1597
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-03-21 16:13:08 +0200

    Code alignment

--- srcs/SPAStarter/SPAStarter/Controllers/PersonsController.cs
+++ srcs/SPAStarter/SPAStarter/Controllers/PersonsController.cs
@@ -1,61 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using SPAStarter.Contracts;
+using SPAStarter.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using SPAStarter.Contracts;
-using SPAStarter.Models;
 
 namespace SPAStarter.Controllers
 {
-    public class PersonsController : ApiControllerBase
-    {
-        public PersonsController(ICodeCamperUow uow)
-        {
-            Uow = uow;
-        }
+	public class PersonsController : ApiControllerBase
+	{
+		public PersonsController(ICodeCamperUow uow)
+		{
+			Uow = uow;
+		}
 
-        #region OData Future: IQueryable<T>
-        //[Queryable]
-        // public IQueryable<Person> Get()
-        #endregion
+		#region OData Future: IQueryable<T>
 
-        // GET /api/persons
-        public IEnumerable<Person> Get()
-        {
-            return Uow.Persons.GetAll()
-                .OrderBy(p => p.FirstName);
-        }
+		//[Queryable]
+		// public IQueryable<Person> Get()
 
-        // GET /api/persons/5
-        public Person Get(int id)
-        {
-            var person = Uow.Persons.GetById(id);
-            if (person != null) return person;
-            throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
-        }
+		#endregion OData Future: IQueryable<T>
 
-        // OData: GET /api/persons/?firstname=\'Hans\''
-        // With OData query syntax we would not need such methods
-        // /api/persons/getbyfirstname?value=Joe1
-        [ActionName("getbyfirstname")]
-        public Person GetByFirstName(string value)
-        {
-            var person = Uow.Persons.GetAll()
-                .FirstOrDefault(p => p.FirstName.StartsWith(value));
+		// GET /api/persons
+		public IEnumerable<Person> Get()
+		{
+			return Uow.Persons.GetAll()
+				.OrderBy(p => p.FirstName);
+		}
 
-            if (person != null) return person;
-            throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
-        }
+		// GET /api/persons/5
+		public Person Get(int id)
+		{
+			var person = Uow.Persons.GetById(id);
+			if (person != null) return person;
+			throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+		}
 
-        // Update an existing person
-        // PUT /api/persons/
-        public HttpResponseMessage Put(Person person)
-        {
-            Uow.Persons.Update(person);
-            Uow.Commit();
-            return new HttpResponseMessage(HttpStatusCode.NoContent);
-        }
+		// OData: GET /api/persons/?firstname=\'Hans\''
+		// With OData query syntax we would not need such methods
+		// /api/persons/getbyfirstname?value=Joe1
+		[ActionName("getbyfirstname")]
+		public Person GetByFirstName(string value)
+		{
+			var person = Uow.Persons.GetAll()
+				.FirstOrDefault(p => p.FirstName.StartsWith(value));
 
-    }
+			if (person != null) return person;
+			throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+		}
+
+		// Update an existing person
+		// PUT /api/persons/
+		public HttpResponseMessage Put(Person person)
+		{
+			Uow.Persons.Update(person);
+			Uow.Commit();
+			return new HttpResponseMessage(HttpStatusCode.NoContent);
+		}
+	}
 }
