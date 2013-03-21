d4b96be7b782e641b3140728c8e6aa3452940872
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2013-03-21 17:09:30 +0200

    Installed some new packages (toastr, durandal, knockuotjs)

--- srcs/SPAStarter/SPAStarter/App_Start/BundleConfig.cs
+++ srcs/SPAStarter/SPAStarter/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Optimization;
+﻿using System;
+using System.Web.Optimization;
 
 namespace SPAStarter
 {
@@ -6,8 +7,14 @@ namespace SPAStarter
 	{
 		public static void RegisterBundles(BundleCollection bundles)
 		{
+			bundles.IgnoreList.Clear();
+			AddDefaultIgnorePatterns(bundles.IgnoreList);
+
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-1.*"));
+						"~/Scripts/jquery-1.9.1.min.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
+						"~/Scripts/toastr.min.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 						"~/Scripts/bootstrap.min.js"));
@@ -19,7 +26,23 @@ namespace SPAStarter
 						"~/Content/bootstrap-responsive.min.css",
 						"~/Content/bootstrap.min.css",
 						"~/Content/font-awesome.min.css",
-						"~/Content/Site.css"));
+						"~/Content/ie10mobile.css",
+						"~/Content/app.css"));
+		}
+
+		public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+		{
+			if (ignoreList == null)
+			{
+				throw new ArgumentNullException("ignoreList");
+			}
+
+			ignoreList.Ignore("*.intellisense.js");
+			ignoreList.Ignore("*-vsdoc.js");
+
+			//ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+			//ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+			//ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
 		}
 	}
 }
